3f510457ab711921123dfa2f4882d223f2f5c429
moumitaseemaa <moumitaseemaa@gmail.com>
2026-03-16 14:31:15 +0600

    Fixed merge conflicts from main

--- src/Componants/CommonLayouts.jsx
+++ src/Componants/CommonLayouts.jsx
@@ -9,11 +9,7 @@ const CommonLayouts = () => {
       <Sidebar />
       <div className="flex-1 flex flex-col min-w-0 bg-[#FFFFFF] ">
         <Header />
-<<<<<<< HEAD:src/components/CommonLayouts.jsx
-        <div className="flex-1 overflow-y-auto p-12 text-black">
-=======
         <div className="bg-[#FFFFFF] p-12 text-black">
->>>>>>> f77747e99fe020f763d248f1a72c49620cfc1d60:src/Componants/CommonLayouts.jsx
           <Outlet />
         </div>
       </div>
