554d6436616c6e2455ef2904bd51fc5a998c6250
moumitaseemaa <moumitaseemaa@gmail.com>
2026-03-17 13:31:03 +0600

    routing updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,37 @@
 
 // import { useContext } from "react"
 
-
-
 import { Routes, Route } from "react-router";
 
 import Dashboard from "./pages/Dashboard/Dashboard";
 import CommonLayouts from "./Componants/CommonLayouts";
 import { ThemeProvider } from "./Componants/Themes/ThemeProvider";
+import ProjectManagement from "./pages/ProjectManagement/ProjectManagement";
+import TodoManagement from "./pages/TodoManagement/TodoManagement";
+import SalesOPCollaboration from "./pages/SalesOPCollaboration/SalesOPCollaboration";
+import WorkForce from "./pages/WorkForce/WorkForce";
+import Monitoring from "./pages/Monitoring/Monitoring";
+import Diagram from "./pages/Diagram/Diagram";
+import Settings from "./pages/Settings/Settings";
 
 
-
-const App = () =>{
-  
-  
-  return(
-
-<ThemeProvider>
-    <Routes>
-      <Route element={<CommonLayouts/>}>
-        <Route index element={<Dashboard/>} />
-        {/* <Route path="/settings/profile" element={<EdittProfilePage />} /> */}
-      </Route>
-    </Routes>
+const App = () => {
+  return (
+    <ThemeProvider>
+      <Routes>
+        <Route element={<CommonLayouts />}>
+          <Route index element={<Dashboard />} />
+          <Route path="/project" element={<ProjectManagement />} />
+          <Route path="/todo" element={<TodoManagement />} />
+          <Route path="/sales" element={<SalesOPCollaboration />} />
+          <Route path="/workforce" element={<WorkForce />} />
+          <Route path="/monitoring" element={<Monitoring />} />
+          <Route path="/diagram" element={<Diagram />} />
+          <Route path="/settings" element={<Settings />} />
+          {/* <Route path="/settings/profile" element={<EdittProfilePage />} /> */}
+        </Route>
+      </Routes>
     </ThemeProvider>
   );
 };
 export default App;
-
