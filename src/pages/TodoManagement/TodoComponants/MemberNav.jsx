4094325b79da0f9eb887d3f3a74c163876b831e4
TaheeraTabassum <tahiratabassumfarha@gmail.com>
2026-03-27 13:02:59 +0600

    issue detected

---
+++ src/pages/TodoManagement/TodoComponants/MemberNav.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { NavLink } from "react-router";
+
+export default function MemberNav() {
+  return (
+    <div>
+      <ul className="flex  gap-6 md:gap-10  my-8">
+        <li>
+          <NavLink
+            to="/todo/member/assignedtask"
+            className={({ isActive }) => (isActive ? "text-orange-400 dark:text-teal-300 underline underline-offset-6  " : "")}
+          >
+           Assigned Task
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/todo/member/selflearning"
+            className={({ isActive }) => (isActive ? "text-orange-400 underline underline-offset-6  dark:text-teal-300" : "")}
+          >
+            Self Learning
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+}
